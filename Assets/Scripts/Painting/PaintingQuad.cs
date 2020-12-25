85f2e4b822faf5dacc957b7ea48497390748f8ab
vegechick123 <destiny173@163.com>
2020-12-25 22:28:13 +0800

    Paint Temp

---
+++ Assets/Scripts/Painting/PaintingQuad.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintingQuad : MonoBehaviour
+{
+    public float beginDissolveTime=2f;
+    public float endDissolveTime = 5f;
+    public float currentTime = 0f;
+    public Color color;
+    public Material material;
+    private void Start()
+    {
+        material = GetComponent<MeshRenderer>().material;
+        material.SetColor("BaseColor",color);
+        Vector2Int location=new Vector2Int(Random.Range(0, 4), Random.Range(0, 4));
+        material.SetVector("PaintCenter",new Vector4(location.x, location.y, 0,0));
+    }
+    private void Update()
+    {
+        currentTime += Time.deltaTime;
+        if (currentTime > endDissolveTime)
+            Destroy(gameObject);
+
+    }
+    
+
+}
