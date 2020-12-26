6287addbf6bf7c674645afd817c992b4d725f22f
vegechick123 <destiny173@163.com>
2020-12-27 01:42:48 +0800

    CameraFloating

--- Assets/Scripts/CameraFloating.cs
+++ Assets/Scripts/CameraFloating.cs
@@ -5,18 +5,51 @@ using UnityEngine;
 public class CameraFloating : MonoBehaviour
 {
     public Transform target;
-    public Vector3 center;
-    public float originRotationY;
-    public float scale=1;
+    private Vector3 center;
+    private Vector3 originEuler;
+    private Vector2 originFoward;
+    private Vector3 targetEuler;
+    private float currentY { get { return transform.rotation.eulerAngles.y; } }
+    public float scale = 1;
+    public float maxDegreePerSecond=1f;
+    public float fade=0.5f;
+    public float floatScale=0.1f;
+    public float floatSpeed = 0.1f;
+    public Vector3 floatTime;//= new Vector3(Random.Range(0,2*Mathf.PI), Random.Range(0, 2 * Mathf.PI), Random.Range(0, 2 * Mathf.PI));
     private void Start()
     {
         center = transform.position;
-        originRotationY = transform.rotation.eulerAngles.y;
+        originEuler = transform.rotation.eulerAngles;
+        originFoward = new Vector3(transform.forward.x, transform.forward.z).normalized;
+        center = transform.position;
+        floatTime = new Vector3(Random.Range(0, 2 * Mathf.PI), Random.Range(0, 2 * Mathf.PI), Random.Range(0, 2 * Mathf.PI));
     }
     private void Update()
     {
-        Vector3 targetDirection = (target.position - transform.position).normalized;
-        transform.forward = targetDirection;
-        //transform.rotation=transform.forward
+        floatTime.x += Time.deltaTime+Random.Range(0, Time.deltaTime);
+        floatTime.y += Time.deltaTime + Random.Range(0, Time.deltaTime);
+        floatTime.z += Time.deltaTime + Random.Range(0, Time.deltaTime);
+
+        transform.position = floatScale*new Vector3(Mathf.Sin(floatTime.x), Mathf.Sin(floatTime.y), Mathf.Sin(floatTime.z)) *Mathf.Sin(floatSpeed*Time.time)+center;
+        SetTargetEuler();
+
+        float next = Mathf.Lerp(currentY, targetEuler.y ,fade);
+
+        transform.rotation = Quaternion.Euler(new Vector3(originEuler.x,next, originEuler.z));
+
+    }
+    void SetTargetEuler()
+    {
+        Vector3 targetDirection = (target.position - transform.position);
+
+        Vector2 targetForward = new Vector2(targetDirection.x, targetDirection.z).normalized;
+
+        float angle = Vector2.SignedAngle(targetForward, originFoward);
+
+        float targetAngle = originEuler.y + angle;
+
+        angle = Mathf.Lerp(originEuler.y, targetAngle, scale);
+
+        targetEuler = new Vector3(originEuler.x, angle, originEuler.z);
     }
 }
