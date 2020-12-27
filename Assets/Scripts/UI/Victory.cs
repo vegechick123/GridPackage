5634ef4644049d6d9691dcae59e84b9e2d3f9a94
TZTIMINO <52661236+TZTIMINO@users.noreply.github.com>
2020-12-27 19:00:33 +0800

    new

---
+++ Assets/Scripts/UI/Victory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Victory : MonoBehaviour, UIBase
+{
+    public void OnClik1()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UIManager.instance.LevelIndex++);
+    }
+
+    public void OnClik2()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnClik3()
+    {
+        throw new NotImplementedException();
+    }
+}
+
